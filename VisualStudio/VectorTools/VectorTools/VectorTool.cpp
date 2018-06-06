b9c4f5adaf81689ad1af55e99629b932b93c7ca9
Luca Di Sera <bloodtype.sigma@gmail.com>
2018-06-06 18:27:24 +0200

    - Continued the work on the vectorTool context

--- VisualStudio/VectorTools/VectorTools/VectorTool.cpp
+++ VisualStudio/VectorTools/VectorTools/VectorTool.cpp
@@ -1,4 +1,9 @@
 #include "VectorTool.h"
+#include "VectorLocator.h"
+
+#include <maya/MFnDagNode.h>
+#include <maya/MDagPath.h>
+#include <maya/MTransformationMatrix.h>
 
 const MString START_HELP_STRING{ "VectorTool: Click to pick the origin point for the vector" };
 const MString END_HELP_STRING{ "VectorTool: Click to pick the end point for the vector" };
@@ -31,7 +36,30 @@ MStatus VectorTool::doPress(MEvent & event)
 			setHelpString(END_HELP_STRING);
 		}
 		else {
-			
+			//Create the locators and set them to the correct world space coordinates
+			MFnDagNode baseLocatorFn{};
+			baseLocatorFn.create("locator");
+
+			MTransformationMatrix baseLocatorMatrix{ baseLocatorFn.transformationMatrix() };
+			baseLocatorMatrix.setTranslation(MVector(basePoint), MSpace::kWorld);
+
+			MFnDagNode endLocatorFn{};
+			endLocatorFn.create("locator");
+
+			MTransformationMatrix endLocatorMatrix{ endLocatorFn.transformationMatrix() };
+			endLocatorMatrix.setTranslation(MVector(basePoint), MSpace::kWorld);
+
+			//Create the vectorLocator and connects the locators to its input
+			MFnDagNode vectorNodeFn{};
+			vectorNodeFn.create("VectorLocator");
+
+			//Gets a plug to the needed attributes
+			MObject vectorNode{ vectorNodeFn.dagPath().node() };
+			MPlug vectorLocatorBasePointAttribute{ vectorNode, VectorLocator::basePoint  };
+			MPlug vectorLocatorEndPointAttribute{ vectorNode, VectorLocator::endPoint };
+
+			MObject baseLocatorNode{ baseLocatorFn.dagPath().node() };
+
 		}
 	}
 
